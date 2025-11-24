e42e49be198a533d19cfb18c1db2738bf5d4b2b0
૮₍ ´• ˕ •` ₎ა <167632296+strawpies@users.noreply.github.com>
2025-11-23 21:11:06 -0300

    Update ElectionData.java

--- src/br/unicamp/ft/si400/votacao/common/ElectionData.java
+++ src/br/unicamp/ft/si400/votacao/common/ElectionData.java
@@ -1,14 +1,9 @@
 package br.unicamp.ft.si400.votacao.common;
 
 import java.util.List;
-/**
- * Primitiva de rede (Record) que encapsula os dados da eleição.
- * Enviada do Servidor para o Cliente.
- * * Usamos um 'record' (Java 16+) para imutabilidade e concisão.
- */
+//encapsula os dados da eleicao
 public record ElectionData(
     String question, 
     List<String> options
 ) implements NetworkMessage {
-    // 'record' implementa automaticamente Serializable se seus campos forem
 }
