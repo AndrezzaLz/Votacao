0defed0fb6e7a906da2eca3cd05210dcc3b5e29a
૮₍ ´• ˕ •` ₎ა <167632296+strawpies@users.noreply.github.com>
2025-11-23 21:13:07 -0300

    Update ServerMain.java

--- src/br/unicamp/ft/si400/votacao/server/ServerMain.java
+++ src/br/unicamp/ft/si400/votacao/server/ServerMain.java
@@ -3,11 +3,7 @@ package br.unicamp.ft.si400.votacao.server;
 import javax.swing.SwingUtilities;
 import br.unicamp.ft.si400.votacao.utils.Info;
 
-/**
- * Ponto de entrada (main) para a aplicação do Servidor.
- * Utiliza a lógica do SimpleGUI_Start para iniciar a GUI
- * na Event Dispatch Thread (EDT) de forma segura.
- */
+
 public class ServerMain implements Runnable {
     
     public static final int EXIT_SUCCESS = 0;
@@ -15,7 +11,6 @@ public class ServerMain implements Runnable {
 
     public static void main(String[] args) {
         try {
-            // Inicia a GUI do servidor na thread correta
             SwingUtilities.invokeLater(new ServerMain());
         } catch (Exception exceptionValue) {
             System.err.println(Info.getShortVersion() + " encontrou um erro e não pode continuar.\nO erro foi: " + exceptionValue.getMessage());
@@ -26,7 +21,6 @@ public class ServerMain implements Runnable {
 
     @Override
     public void run() {
-        // Cria e exibe a janela principal do servidor
         ServerGUI program = new ServerGUI(Info.getLongVersion());
         program.go();
     }
