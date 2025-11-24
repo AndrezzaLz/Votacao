b631a991471c8fb5a1b2399c0e5f7fb3e3369e42
૮₍ ´• ˕ •` ₎ა <167632296+strawpies@users.noreply.github.com>
2025-11-23 21:12:18 -0300

    Update ClientHandler.java

--- src/br/unicamp/ft/si400/votacao/server/ClientHandler.java
+++ src/br/unicamp/ft/si400/votacao/server/ClientHandler.java
@@ -10,10 +10,6 @@ import br.unicamp.ft.si400.votacao.common.ElectionData;
 import br.unicamp.ft.si400.votacao.common.StatusUpdate;
 import br.unicamp.ft.si400.votacao.common.NetworkMessage;
 
-/**
- * Lida com a comunicação de um único cliente.
- * Roda em sua própria thread (virtual).
- */
 class ClientHandler implements Runnable {
 
     private final Socket socket;
@@ -31,44 +27,35 @@ class ClientHandler implements Runnable {
     @Override
     public void run() {
         try {
-            // Configura os Object Streams
             outStream = new ObjectOutputStream(socket.getOutputStream());
             inStream = new ObjectInputStream(socket.getInputStream());
 
-            // 1. Envia os dados da eleição para o cliente
             ElectionData data = server.getElectionData();
             sendMessage(data);
 
-            // 2. Aguarda o voto do cliente
             NetworkMessage msg = (NetworkMessage) inStream.readObject();
 
             if (msg instanceof Vote vote) {
-                // 3. Recebeu um voto, agora valida
                 StatusUpdate.Status status;
                 
-                // 3.1. Valida CPF
                 if (!CPFValidator.validate(vote.cpf())) {
                     status = StatusUpdate.Status.INVALID_CPF;
                     gui.log("Cliente " + socket.getInetAddress() + " enviou CPF inválido.");
                 
-                // 3.2. Valida duplicidade
                 } else if (!server.registerVote(vote.cpf(), vote.selectedOption())) {
                     status = StatusUpdate.Status.ALREADY_VOTED;
                     gui.log("Cliente " + socket.getInetAddress() + " (CPF: " + vote.cpf() + ") tentou votar novamente.");
                 
-                // 3.3. Voto aceito
                 } else {
                     status = StatusUpdate.Status.VOTE_ACCEPTED;
                 }
 
-                // 4. Envia a resposta (StatusUpdate) para o cliente
                 sendMessage(new StatusUpdate(status, "Obrigado por votar."));
             }
 
         } catch (IOException  | ClassNotFoundException e) {
             gui.log("Cliente " + socket.getInetAddress() + " desconectou: " + e.getMessage());
         } finally {
-            // 5. Encerra a conexão
             try {
                 if (socket != null && !socket.isClosed()) {
                     socket.close();
@@ -79,9 +66,7 @@ class ClientHandler implements Runnable {
         }
     }
 
-    /**
-     * Envia um objeto serializável (primitiva) para o cliente.
-     */
+
     private void sendMessage(NetworkMessage msg) {
         try {
             outStream.writeObject(msg);
